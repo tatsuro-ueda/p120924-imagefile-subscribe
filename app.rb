e59d2b7e2db828e279323bf56e29a6e7eeb9fac0
weed <weed_7777@yahoo.co.jp>
2012-09-25 11:02:42 +0900

    ファイルアップロード

--- app.rb
+++ app.rb
@@ -3,3 +3,12 @@ require 'sinatra'
 get '/' do
   'Hello World !'
 end
+
+# アップロードされたファイルを返す
+put '/upload' do
+  if params[:file]
+    content_type params[:file][:type]
+    f = params[:file][:tempfile]
+    f.read f.size
+  end
+end
